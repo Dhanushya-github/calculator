e482ab1b0faeb815abb300579d0a11f86988d4e8
dhanush <dhanush@gmail.com>
2024-09-01 23:17:10 +0530

    sixth commit

--- python2.py
+++ python2.py
@@ -1,15 +1,4 @@
-import sys
-
-if len(sys.argv) != 3:
-    print("Usage: python addition.py <first_number> <second_number>")
-    sys.exit(1)
-
-try:
-    first_number = float(sys.argv[1])
-    second_number = float(sys.argv[2])
-except ValueError:
-    print("Please enter valid numbers.")
-    sys.exit(1)
-
-result = first_number + second_number
-print(f"The result is: {result}")
+x = input("Type a number:")
+y = input("Type another number:")
+sum = int(x) + int(y)
+print("The sum is", sum)
